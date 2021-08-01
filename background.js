169bcd122bf8287614225d60e5f205626c9acd55
Perez Alan <diegoalanperezz@gmail.com>
2021-07-31 22:50:35 -0300

    fixing regex

--- background.js
+++ background.js
@@ -10,30 +10,41 @@ const femaleWords = [
     "programadora",
     "ingeniera"
 ];
+const runBtn = document.getElementById("runBtn");
 
 function replaceWords(listWords, colorIdentifier) {
     let counter = 0;
     let body = document.body;
     listWords.forEach(element => {
-        console.log("typeof =>", typeof element)
-        const reg = new RegExp("^" + element + "$", "gim");
+        const reg = new RegExp(`\\b${element}\\b`, "gim");
 
-        console.log("REG TEST", element, reg.test(body.innerHTML))
         if (reg.test(body.innerHTML)) {
             counter += body.innerHTML.match(reg).length;
         }
+        console.log(body.innerHTML)
         body.innerHTML = body.innerHTML.replace(reg, `<mark style="background-color:${colorIdentifier};"> ${element} </mark>`)
     });
     return counter;
 }
 
-function setValues(elementId, list) {
-    console.log("EJECUTO CON", elementId);
-    const quantity = replaceWords(list, '#f8d371');
+function setValues(elementId, list, colorIdentifier) {
+    const quantity = replaceWords(list, colorIdentifier);
     document.getElementById(elementId).innerHTML = `${elementId} - ${quantity}`;
 }
-document.addEventListener("DOMContentLoaded", function(){
-    console.log("ya cargue el dom")
-    setValues("femaleWords", femaleWords);
-    setValues("maleWords", maleWords)
+
+function init() {
+    setValues("femaleWords", femaleWords, 'red');
+    setValues("maleWords", maleWords, 'blue');
+    console.log(document.body.innerHTML);
+}
+
+
+runBtn.addEventListener("click", () => {
+    let [tab] = chrome.tabs.query({ active: true, currentWindow: true });
+    console.log(document.body.innerHTML)
+    chrome.scripting.executeScript({
+        target: { tabId: tab.id },
+        files: ['background.js'],
+        function: init(),
+    });
 });
