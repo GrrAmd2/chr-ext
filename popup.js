497549f0a9414cdda071a2f3ced2798044e0d5fd
Perez Alan <diegoalanperezz@gmail.com>
2021-08-04 10:37:55 -0300

    frontend

--- popup.js
+++ popup.js
@@ -3,9 +3,26 @@ const maleCounter = document.querySelector("#maleCounter");
 const femaleCounter = document.querySelector("#femaleCounter");
 // Share button
 const shareBtn = document.querySelector("#shareBtn");
+const buttonBack = document.querySelector("#buttonBack");
+
+buttonBack.addEventListener("click", () =>{
+	const shareSection = document.getElementById("shareSection");
+	const counterSection = document.getElementById("counterSection");
+	shareSection.style.display = 'none';
+	counterSection.style.display = 'block';
+})
+
 shareBtn.addEventListener("click", () => {
-	console.log("Compartido por cierta applicacion");
+	showShareSection();
 });
+
+function showShareSection(){
+	const shareSection = document.getElementById("shareSection");
+	const counterSection = document.getElementById("counterSection");
+	shareSection.style.display = 'block';
+	counterSection.style.display = 'none';
+}
+
 
 // Send message to run word-filter
 chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
